2667c17a91ad593705725d73a16b4090f5bafa77
Christian Burgard <christian.nooma@moss.earth>
2021-04-27 09:31:51 -0300

    add swagger on model

--- swagger.js
+++ swagger.js
@@ -19,15 +19,6 @@ const doc = {
       type: 'apiKey',
       name: 'api_key',
       in: 'header'
-    },
-    petstore_auth: {
-      type: 'oauth2',
-      authorizationUrl: 'https://petstore.swagger.io/oauth/authorize',
-      flow: 'implicit',
-      scopes: {
-        read_pets: 'read your pets',
-        write_pets: 'modify pets in your account'
-      }
     }
   },
   tags: [
